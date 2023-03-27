fbcd82423d21218a80e0d5e4d402d8f77ece04db
simondar <simd73@gmail.com>
2023-03-27 13:17:33 +0200

    Backup

---
+++ simon_testbench/Lab6_BB/Lab6/software/Gitar_Hero_NIOS/obj/default/CheckSignal.d
@@ -0,0 +1,11 @@
+obj/default/CheckSignal.o: CheckSignal.c CheckSignal.h \
+ ../Gitar_Hero_NIOS_bsp/system.h ../Gitar_Hero_NIOS_bsp/linker.h \
+ ../Gitar_Hero_NIOS_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Gitar_Hero_NIOS_bsp//HAL/inc/io.h \
+ ../Gitar_Hero_NIOS_bsp//HAL/inc/alt_types.h
+CheckSignal.h:
+../Gitar_Hero_NIOS_bsp/system.h:
+../Gitar_Hero_NIOS_bsp/linker.h:
+../Gitar_Hero_NIOS_bsp//drivers/inc/altera_avalon_pio_regs.h:
+../Gitar_Hero_NIOS_bsp//HAL/inc/io.h:
+../Gitar_Hero_NIOS_bsp//HAL/inc/alt_types.h:
---
+++ simon_testbench/Lab6_BB/Lab6/software/Gitar_Hero_NIOS/obj/default/main.d
@@ -0,0 +1,11 @@
+obj/default/main.o: main.c main.h ../Gitar_Hero_NIOS_bsp/system.h \
+ ../Gitar_Hero_NIOS_bsp/linker.h \
+ ../Gitar_Hero_NIOS_bsp//drivers/inc/altera_avalon_pio_regs.h \
+ ../Gitar_Hero_NIOS_bsp//HAL/inc/io.h \
+ ../Gitar_Hero_NIOS_bsp//HAL/inc/alt_types.h
+main.h:
+../Gitar_Hero_NIOS_bsp/system.h:
+../Gitar_Hero_NIOS_bsp/linker.h:
+../Gitar_Hero_NIOS_bsp//drivers/inc/altera_avalon_pio_regs.h:
+../Gitar_Hero_NIOS_bsp//HAL/inc/io.h:
+../Gitar_Hero_NIOS_bsp//HAL/inc/alt_types.h:
